d97a3e121a50332fc3400a9f788e6cd2714dce5d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-27 08:52:56 -0600

    feat: Added interface, abstract and concrete classes draft

---
+++ Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        // One-parameter constructor
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        // Return a string replacement of Shape object and properties
+        public override string ToString() => $"{Name}";
+
+        // Abstract method to be overridden by derived classes
+        public abstract decimal Area();  // No implementation 
+    }
+}
