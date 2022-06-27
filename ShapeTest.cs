3fd81d5c69b4879d81cc6de0d8a8dc7c792c19b7
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-27 08:30:36 -0600

    Add project files.

---
+++ ShapeTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class ShapeTest
+    {
+        static void Main(string[] args)
+        {
+
+            var circle = new TwoDimensionalShape("Radius");
+            var square = new TwoDimensionalShape("Length");
+
+            var sphere = new ThreeDimensionalShape("Radius");
+            var cube = new ThreeDimensionalShape("Length");
+
+            // Calculation for a circle
+            Console.WriteLine($"The area of a {circle}: {Math.PI} * {Math.Pow(radius, 2)} = {area}");
+            Console.WriteLine();
+
+            // Calculation for a square
+            Console.WriteLine($"The area of a {square}: {length} * {length} = {area}");
+            Console.WriteLine();
+
+            // Calculations for a sphere
+            Console.WriteLine($"The area of a {sphere}: {Math.PI} * {Math.Pow(radius, 2)} = {area}");
+            Console.WriteLine();
+
+            // Calculations for a cube
+            Console.WriteLine($"The area of a {cube}: {length} * {length} = {area}");
+            Console.WriteLine();
+        }
+    }
+}
