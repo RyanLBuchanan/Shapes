3d485b961e157d9af786c95e3ec5495d72a6b0e1
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-27 09:35:51 -0600

    feat: Added extended ThreeDimensionalShape class

---
+++ ThreeDimensionalShape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class ThreeDimensionalShape : Shape , IGeometry
+    {
+        public string name;
+
+        // One parameter constructor
+        public ThreeDimensionalShape(string name)
+        {
+            Name = name;
+        }
+        // Property to get set name
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+
+        // Return a string replacement of Shape object and properties
+        public override string ToString() => $"{name}";
+
+        // Abstract method to be overridden by derived classes
+        public abstract decimal Area();  // No implementation 
+
+        // Abstract method to be overridden by derived classes
+        public abstract decimal Volume();  // No implementation 
+    }
+}
